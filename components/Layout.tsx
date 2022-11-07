215cf4b4844a66296e600d8146709658b2a1d295
arya perdana <arya.perdana@happy5.co>
2022-11-07 08:29:11 +0700

    crypto market ui

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,6 +1,17 @@
 import React, { ReactNode } from "react";
 import Head from "next/head";
-import { Container } from "@chakra-ui/react";
+import {
+  Box,
+  Container,
+  Flex,
+  Heading,
+  Icon,
+  Link,
+  Spacer,
+  Text,
+} from "@chakra-ui/react";
+import ColorModeSwitcher from "./ColorModeSwitcher";
+import { FaGithub } from "react-icons/fa";
 
 interface LayoutProps {
   children?: ReactNode;
@@ -20,7 +31,51 @@ const Layout = ({
         <meta charSet="utf-8" />
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
-      <Container></Container>
+      <Container maxW={{ xl: "1200px" }} h="100vh">
+        <Flex pt={10} pb={24}>
+          <Box>
+            <Heading>{title}</Heading>
+            <Text
+              bgGradient="linear(to-l, #7928CA,#FF0080)"
+              bgClip="text"
+              fontWeight="extrabold"
+            >
+              {subTitle}
+            </Text>
+          </Box>
+
+          <Spacer />
+          <ColorModeSwitcher />
+        </Flex>
+
+        {children}
+        <Box
+          position="fixed"
+          bottom="0"
+          left="0"
+          zIndex="sticky"
+          borderTop="1px"
+          borderColor="gray.200"
+          py={2}
+          w="full"
+        >
+          <footer>
+            <Container maxW={{ xl: "1200px" }}>
+              <Flex>
+                <Box>
+                  <span>React Query Example</span>
+                </Box>
+                <Spacer />
+                <Box>
+                  <Link href="https://github.com/ardana26" isExternal mx={4}>
+                    <Icon as={FaGithub} w={6} h={6} color="#24292e" />
+                  </Link>
+                </Box>
+              </Flex>
+            </Container>
+          </footer>
+        </Box>
+      </Container>
     </>
   );
 };
